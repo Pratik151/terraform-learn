b9bf54356177e95d8e8978402a291b98135933c6
Pratik151 <pratikpatel15133@gmail.com>
2023-09-23 16:24:00 +0530

    Start web server on instance and create SG for ingress on 8080

--- main.tf
+++ main.tf
@@ -8,4 +8,19 @@ resource "aws_instance" "ec2-server-1" {
   tags = {
     Name = "terraform-example"
   }
+  user_data = <<-EOF
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  user_data_replace_on_change = true
+  vpc_security_group_ids = [aws_security_group.ec2-security-group.id]
+}
+
+resource "aws_security_group" "ec2-security-group" {
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
